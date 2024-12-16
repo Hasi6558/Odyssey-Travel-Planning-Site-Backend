489123b1260905b23c2db4c4fe080ad2a0160e00
Thusithakit <thusitha@kongcepts.info>
2024-12-17 05:05:59 +0530

    created basic CRUD operations for users

---
+++ src/main/java/com/example/OdysseyTravelPlanningWebsiteBackendApplication/repo/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.OdysseyTravelPlanningWebsiteBackendApplication.repo;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.example.OdysseyTravelPlanningWebsiteBackendApplication.model.User;
+
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
---
+++ src/main/java/com/example/OdysseyTravelPlanningWebsiteBackendApplication/service/UserService.java
@@ -0,0 +1,18 @@
+package com.example.OdysseyTravelPlanningWebsiteBackendApplication.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.OdysseyTravelPlanningWebsiteBackendApplication.model.User;
+
+public interface UserService {
+    User createUser(User user);
+
+    List<User> getAllUsers();
+
+    Optional<User> getUserById(String id);
+
+    User updateUser(String id, User userDetails);
+
+    void deleteUser(String id);
+}
