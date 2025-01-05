2548db226db5cc1a51d9aac6edc3d0892cea4cc9
hasindu <hasindus48@gmail.com>
2025-01-05 10:18:40 +0530

    implemented searchToursByCity method

--- src/main/java/com/example/OdysseyTravelPlanningWebsiteBackendApplication/repo/RestaurantRepository.java
+++ src/main/java/com/example/OdysseyTravelPlanningWebsiteBackendApplication/repo/RestaurantRepository.java
@@ -5,8 +5,12 @@ import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.example.OdysseyTravelPlanningWebsiteBackendApplication.model.Restaurant;
+import org.springframework.data.mongodb.repository.Query;
 
 public interface RestaurantRepository extends MongoRepository<Restaurant, String> {
+    @Query("{ 'location_city': { $regex: ?0, $options: 'i' } }")
+    List<Restaurant> findByCityContaining(String searchTerm);
+
 
    
 
