e4f957ffe56bc0be4a5c412eb0862aa1d6278b6a
hasindu <hasindus48@gmail.com>
2025-01-05 22:31:59 +0530

    added WebConfig.java

--- src/main/java/com/example/OdysseyTravelPlanningWebsiteBackendApplication/WebConfig.java
+++ src/main/java/com/example/OdysseyTravelPlanningWebsiteBackendApplication/WebConfig.java
@@ -1,4 +1,15 @@
 package com.example.OdysseyTravelPlanningWebsiteBackendApplication;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allow all endpoints
+                .allowedOrigins("https://Hasi6558.github.io") // Allow requests only from your GitHub Pages site
+                .allowedMethods("GET", "POST", "PUT", "DELETE"); // Allow specific HTTP methods
+    }
 }
